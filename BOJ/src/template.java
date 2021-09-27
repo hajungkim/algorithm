1d24088d283c818be4b375ab6d92aecc03aaf4d6
hajungkim <haha950903@naver.com>
2021-09-27 23:09:48 +0900

    add 템플릿

---
+++ BOJ/src/template.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+/*
+
+ */
+public class template {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+    }
+}
