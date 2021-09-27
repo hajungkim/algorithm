1cf6c3a258ee9d0f81fc2e888c4aed5d315d2c57
hajungkim <haha950903@naver.com>
2021-09-27 23:10:13 +0900

    add boj 1476

--- BOJ/src/bruteforce/Main_1476.java
+++ BOJ/src/bruteforce/Main_1476.java
@@ -1,7 +1,28 @@
 package bruteforce;
-
+import java.util.*;
+import java.io.*;
+/*
+날짜 계산
+https://www.acmicpc.net/problem/1476
+ */
 public class Main_1476 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int E = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int result = 1, e = 1, s = 1, m = 1;
+        while (true) {
+            if (e == E && s == S && m == M) break;
+            e++;
+            s++;
+            m++;
+            if (e > 15) e = 1;
+            if (s > 28) s = 1;
+            if (m > 19) m = 1;
+            result++;
+        }
+        System.out.println(result);
     }
 }
