3412c70df71db16fb3a58f32590a2909f55da259
hajungkim <haha950903@naver.com>
2021-10-02 00:29:43 +0900

    update template

--- BOJ/src/template.java
+++ BOJ/src/template.java
@@ -1,8 +1,8 @@
-import java.util.*;
-import java.io.*;
 /*
 
  */
+import java.util.*;
+import java.io.*;
 public class template {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
